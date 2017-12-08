9ea2c3fcd8c6cfcc73d668b83fbbb798a5b41318
Chris Gullickson <cgullickson5@gmail.com>
2017-12-08 18:40:46 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+class Appointment
+  attr_accessor :patient, :doctor
+
+  def initialize (date, doctor)
+    @date = date
+    @doctor = doctor
+  end
+
+  def patient
+
+  end
+
+  def doctor
+
+  end    
+
+end
---
+++ lib/patient.rb
@@ -0,0 +1,12 @@
+class Patient
+  attr_accessor :name, :appointment
+
+  def initialize (name)
+    @name = name
+  end
+
+  def add_appointment
+
+  end
+  
+end
